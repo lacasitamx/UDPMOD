79e1e2c0b4e5277c107ecbe8a494819c33089f73
LaCasitaMx <69657855+lacasitamx@users.noreply.github.com>
2023-10-28 12:13:26 -0700

    Add files via upload

--- install-udp.sh
+++ install-udp.sh
@@ -787,22 +787,22 @@ get_latest_version() {
 
 download_hysteria() {
 	#version=$(curl -Ls "https://api.github.com/repos/HyNetwork/Hysteria/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-	version=app/v2.0.4
+	version="v1.3.5"
 #    if [[ ! -n "$version" ]]; then
     #    red "No se pudo detectar la versión de Hysteria. Es posible que se haya excedido el límite de la API de Github. Vuelve a intentarlo más tarde."
       #  exit 1
  #   fi
         get_arch=`arch`
     if [ $get_arch = "x86_64" ];then
-        wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/v2.0.4/hysteria-linux-amd64
+        wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/$version/hysteria-linux-amd64
     elif [ $get_arch = "aarch64" ];then
-        wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/v2.0.4/hysteria-linux-arm64
+        wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/$version/hysteria-linux-arm64
     elif [ $get_arch = "mips64" ];then
-        wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/v2.0.4/hysteria-linux-mipsle
+        wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/$version/hysteria-linux-mipsle
 	elif [ $get_arch = "s390x" ];then
-		wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/v2.0.4/hysteria-linux-s390x
+		wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/$version/hysteria-linux-s390x
 	elif [ $get_arch = "i686" ];then
-		wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/v2.0.4/hysteria-linux-386
+		wget -q -O /usr/local/bin/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/app/$version/hysteria-linux-386
     else
         echo "Error[OS Message]:${get_arch}\nPlease open a issue to https://github.com/emptysuns/Hi_Hysteria/issues !"
         exit
